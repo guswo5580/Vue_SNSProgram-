5bc069b189071caab3f149094772b1c8b8571497
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-01-07 19:58:22 +0900

    modify review db

--- models/review.js
+++ models/review.js
@@ -4,6 +4,10 @@ module.exports = (sequelize, DataTypes) => (
           type : DataTypes.STRING(140),
           allowNull : false,
       },
+      img: {
+        type: DataTypes.STRING(200),
+        allowNull: true,
+      },
     }, {
       timestamps: true,
       paranoid: true,
